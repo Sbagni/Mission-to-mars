86ba65d53e507b3756fcc3f4a82ba5f0035138f7
Gaurav Agnihotri <sumatibhala@gmail.com>
2020-01-07 14:08:17 -0500

    changes the * to **

--- scraped_mars.py
+++ scraped_mars.py
@@ -8,11 +8,11 @@ import pandas as pd
 
 def init_browser():
 	executable_path = {'executable_path': 'chromedriver.exe'}
-	browser = Browser('chrome', *executable_path, headless=False)
+	browser = Browser('chrome', **executable_path, headless=False)
 
 def scrape_info():
     executable_path = {'executable_path': 'chromedriver.exe'}
-    browser = Browser('chrome', *executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=False)
     
 
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
